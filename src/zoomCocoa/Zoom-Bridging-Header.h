d1efcfeeb31daf9e3dc416e4fbe48458a6dbc5af
C.W. Betts <computers57@hotmail.com>
2021-09-27 20:05:10 -0600

    Trim down the bridging header.

--- src/zoomCocoa/Zoom-Bridging-Header.h
+++ src/zoomCocoa/Zoom-Bridging-Header.h
@@ -2,11 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ZoomClient.h"
-#import "ZoomProtocol.h"
-#import "ZoomClientController.h"
+//#import "ZoomClient.h"
+//#import "ZoomProtocol.h"
+//#import "ZoomClientController.h"
 #import "ZoomStoryOrganiser.h"
 
 #import "ZoomAppDelegate.h"
 
-#import "ifmetabase.h"
+//#import "ifmetabase.h"
