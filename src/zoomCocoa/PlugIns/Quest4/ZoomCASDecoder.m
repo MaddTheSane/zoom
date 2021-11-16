3aefc60fe57eeedaba594693acc369ab6abb0eaa
C.W. Betts <computers57@hotmail.com>
2021-11-16 13:45:29 -0700

    Future-proof the CAS decoder.

--- src/zoomCocoa/PlugIns/Quest4/ZoomCASDecoder.m
+++ src/zoomCocoa/PlugIns/Quest4/ZoomCASDecoder.m
@@ -7,44 +7,58 @@
 
 #import "ZoomCASDecoder.h"
 
+#ifndef __MAC_11_0
+#define __MAC_11_0          110000
+#endif
+
+#define ARRAY @"", @"game", @"procedure", @"room", @"object", @"character", @"text", @"selection", \
+@"define", @"end", @"", @"asl-version", @"game", @"version", @"author", @"copyright", \
+@"info", @"start", @"possitems", @"startitems", @"prefix", @"look", @"out", @"gender", \
+@"speak", @"take", @"alias", @"place", @"east", @"north", @"west", @"south", @"give", \
+@"hideobject", @"hidechar", @"showobject", @"showchar", @"collectable", \
+@"collecatbles", @"command", @"use", @"hidden", @"script", @"font", @"default", \
+@"fontname", @"fontsize", @"startscript", @"nointro", @"indescription", \
+@"description", @"function", @"setvar", @"for", @"error", @"synonyms", @"beforeturn", \
+@"afterturn", @"invisible", @"nodebug", @"suffix", @"startin", @"northeast", \
+@"northwest", @"southeast", @"southwest", @"items", @"examine", @"detail", @"drop", \
+@"everywhere", @"nowhere", @"on", @"anything", @"article", @"gain", @"properties", \
+@"type", @"action", @"displaytype", @"override", @"enabled", @"disabled", \
+@"variable", @"value", @"display", @"nozero", @"onchange", @"timer", @"alt", @"lib", \
+@"up", @"down", @"gametype", @"singleplayer", @"multiplayer", @"", @"", @"", @"", @"", \
+@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", \
+@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", \
+@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"do", @"if", @"got", @"then", @"else", \
+@"has", @"say", @"playwav", @"lose", @"msg", @"not", @"playerlose", @"playerwin", \
+@"ask", @"goto", @"set", @"show", @"choice", @"choose", @"is", @"setstring", \
+@"displaytext", @"exec", @"pause", @"clear", @"debug", @"enter", @"movechar", \
+@"moveobject", @"revealchar", @"revealobject", @"concealchar", @"concealobject", \
+@"mailto", @"and", @"or", @"outputoff", @"outputon", @"here", @"playmidi", @"drop", \
+@"helpmsg", @"helpdisplaytext", @"helpclear", @"helpclose", @"hide", @"show", \
+@"move", @"conceal", @"reveal", @"numeric", @"string", @"collectable", @"property", \
+@"create", @"exit", @"doaction", @"close", @"each", @"in", @"repeat", @"while", \
+@"until", @"timeron", @"timeroff", @"stop", @"panes", @"on", @"off", @"return", \
+@"playmod", @"modvolume", @"clone", @"shellexe", @"background", @"foreground", \
+@"wait", @"picture", @"nospeak", @"animate", @"persist", @"inc", @"dec", @"flag", \
+@"dontprocess", @"destroy", @"beforesave", @"onload", @"", @"", @"", @"", @"", @"", \
+@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @""
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_11_0
+static NSArray<NSString *> * const compilation_tokens = @[ARRAY];
+#else
 static NSString * const compilation_tokens[256] =
-{@"", @"game", @"procedure", @"room", @"object", @"character", @"text", @"selection",
-	@"define", @"end", @"", @"asl-version", @"game", @"version", @"author", @"copyright",
-	@"info", @"start", @"possitems", @"startitems", @"prefix", @"look", @"out", @"gender",
-	@"speak", @"take", @"alias", @"place", @"east", @"north", @"west", @"south", @"give",
-	@"hideobject", @"hidechar", @"showobject", @"showchar", @"collectable",
-	@"collecatbles", @"command", @"use", @"hidden", @"script", @"font", @"default",
-	@"fontname", @"fontsize", @"startscript", @"nointro", @"indescription",
-	@"description", @"function", @"setvar", @"for", @"error", @"synonyms", @"beforeturn",
-	@"afterturn", @"invisible", @"nodebug", @"suffix", @"startin", @"northeast",
-	@"northwest", @"southeast", @"southwest", @"items", @"examine", @"detail", @"drop",
-	@"everywhere", @"nowhere", @"on", @"anything", @"article", @"gain", @"properties",
-	@"type", @"action", @"displaytype", @"override", @"enabled", @"disabled",
-	@"variable", @"value", @"display", @"nozero", @"onchange", @"timer", @"alt", @"lib",
-	@"up", @"down", @"gametype", @"singleplayer", @"multiplayer", @"", @"", @"", @"", @"",
-	@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"",
-	@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"",
-	@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"do", @"if", @"got", @"then", @"else",
-	@"has", @"say", @"playwav", @"lose", @"msg", @"not", @"playerlose", @"playerwin",
-	@"ask", @"goto", @"set", @"show", @"choice", @"choose", @"is", @"setstring",
-	@"displaytext", @"exec", @"pause", @"clear", @"debug", @"enter", @"movechar",
-	@"moveobject", @"revealchar", @"revealobject", @"concealchar", @"concealobject",
-	@"mailto", @"and", @"or", @"outputoff", @"outputon", @"here", @"playmidi", @"drop",
-	@"helpmsg", @"helpdisplaytext", @"helpclear", @"helpclose", @"hide", @"show",
-	@"move", @"conceal", @"reveal", @"numeric", @"string", @"collectable", @"property",
-	@"create", @"exit", @"doaction", @"close", @"each", @"in", @"repeat", @"while",
-	@"until", @"timeron", @"timeroff", @"stop", @"panes", @"on", @"off", @"return",
-	@"playmod", @"modvolume", @"clone", @"shellexe", @"background", @"foreground",
-	@"wait", @"picture", @"nospeak", @"animate", @"persist", @"inc", @"dec", @"flag",
-	@"dontprocess", @"destroy", @"beforesave", @"onload", @"", @"", @"", @"", @"", @"",
-	@"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"", @"" };
+{
+	ARRAY
+};
+#endif
+
+#undef ARRAY
 
 NSArray<NSString*> *CASDecompile (NSData *dat)
 {
 	NSMutableString *cur_line = [NSMutableString string];
 	NSString *tok;
 	int8_t expect_text = 0;
-	unsigned int obfus = 0;
+	uint8_t obfus = 0;
 	unsigned char ch;
 	NSMutableArray *rv = [NSMutableArray array];
 	const unsigned char *s = dat.bytes;
