7735bb3a63d72a822e91be265d875a8b15ac9484
C.W. Betts <computers57@hotmail.com>
2024-10-19 19:44:32 -0600

    Add header guards to MUCConverter.h

--- depends/agil111src_glk/Data Formats/MUCConverter.h
+++ depends/agil111src_glk/Data Formats/MUCConverter.h
@@ -5,6 +5,19 @@
 //  Created by C.W. Betts on 10/15/24.
 //
 
+#ifndef MUCConverter_h
+#define MUCConverter_h
+
 #import <Foundation/Foundation.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern NSData *MUCToRiff(NSURL *theFile, NSError *__autoreleasing*outError);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
