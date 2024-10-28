0eb830234cbdf83e8148ea64361c3e357cdef2bd
C.W. Betts <computers57@hotmail.com>
2024-10-27 21:40:41 -0600

    Update CocoaGLK.

--- depends/agil111src_glk/Data Formats/VOCConverter.m
+++ depends/agil111src_glk/Data Formats/VOCConverter.m
@@ -9,6 +9,32 @@
 
 
 const NSErrorDomain VOCConverterErrorDomain = @"com.github.MaddTheSane.AGT.VOCErrors";
+static const unsigned char ff_voc_magic[21] = "Creative Voice File\x1A";
+
+
+typedef NS_ENUM(uint8_t, VOCBlockType) {
+	kVOCTerminator = 0,
+	kVOCSoundData = 1,
+	kVOCSoundDataContinuation = 2,
+	kVOCSilence = 3,
+	kVOCMarker = 4,
+	kVOCText = 5,
+	kVOCRepeatStart = 6,
+	kVOCRepeatEnd = 7,
+	kVOCExtendedInfo = 8,
+	kVOCSoundDataNew = 9
+};
+
+typedef NS_ENUM(uint16_t, VOCFormat) {
+	kVOCFormatU8 = 0,
+	kVOCFormatCreativeADPCM4_8 = 1,
+	kVOCFormatCreativeADPCM3_8 = 2,
+	kVOCFormatCreativeADPCM2_8 = 3,
+	kVOCFormatS16 = 4,
+	kVOCFormatAlaw = 6,
+	kVOCFormatUlaw = 7,
+	kVOCFormatCreativeADPCM4_16 = 0x200,
+};
 
 
 NSData * _Nullable convertVOCToRIFF(NSURL *filePath, NSError *_Nullable __autoreleasing*  _Nullable error)
